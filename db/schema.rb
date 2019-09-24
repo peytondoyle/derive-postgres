18be7114cdd8168b005c6b3c04e30367c30add0f
p6doyle <p6doyle@gmail.com>
2019-09-24 18:36:48 -0400

    new gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_09_190209) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "destination_interests", force: :cascade do |t|
     t.integer "destination_id"
     t.integer "interest_id"
